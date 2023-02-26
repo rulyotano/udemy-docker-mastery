32563246e13dd3a6be42530ed95e0ee069436fb2
Raul Otaño Hurtado <rulyotano@gmail.com>
2023-02-26 23:27:28 +0100

    swarm 1

---
+++ swarm-app-1/my-answer.sh
@@ -0,0 +1,11 @@
+docker network create -d overlay frontend && docker network create -d overlay backend \
+
+docker service create --name vote -p 80:80 --network frontend --replicas 2 bretfisher/examplevotingapp_vote \
+
+docker service create --name redis --network frontend redis:3.2 \
+
+docker service create --name worker --network frontend --network backend bretfisher/examplevotingapp_worker \
+
+docker service create --name db --network backend -e POSTGRES_HOST_AUTH_METHOD=trust --mount type=volume,source=db-data,target=/var/lib/postgresql/data postgres:9.4 \
+
+docker service create --name result -p 5001:80 --network backend bretfisher/examplevotingapp_result
